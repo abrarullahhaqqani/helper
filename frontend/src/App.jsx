960ca344754a8dc7cb00771fcac82a775d11ece4
abrarullahhaqqani <abrarullahhaqqani@gmail.com>
2025-06-29 01:15:46 +0530

    Routing of signin and signup page done in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import Signin from "./pages/Signin";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
 };
 
 export default App;
