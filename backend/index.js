609a53542008b083ab1cd731f58d8330f3002961
abrarullahhaqqani <129473479+abrarullahhaqqani@users.noreply.github.com>
2025-07-01 23:54:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,17 @@
-import express from "express";
-import dotenv from "dotenv";
-dotenv.config();
-import connectDb from "./config/db.js";
-import authRouter from "./routes/auth.routes.js";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import userRouter from "./routes/user.routes.js";
-import geminiResponse from "./gemini.js";
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://helper1-5hh1.onrender.com",
+];
 
-const app = express();
 app.use(
   cors({
-    origin: "https://helper1-5hh1.onrender.com",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
-const port = process.env.PORT || 5000;
-//middleware to read body
-app.use(express.json());
-//middleware for cookie-parser
-app.use(cookieParser());
-
-// Optional: Root route
-// app.get("/", (req, res) => {
-//   res.send("API Server is Running ✅");
-// });
-
-//middleware
-app.use("/api/auth", authRouter);
-
-//API for user
-
-app.use("/api/user", userRouter);
-// app.get("/", async (req, res) => {
-//   let prompt = req.query.prompt;
-//   let data = await geminiResponse(prompt);
-//   res.json(data);
-// });
-app.listen(port, () => {
-  connectDb();
-  console.log("App is running");
-});
