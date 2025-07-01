47a20d3c3458b1db9d54dc505ab666a47d572cac
abrarullahhaqqani <129473479+abrarullahhaqqani@users.noreply.github.com>
2025-07-01 23:39:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://helper1-5hh1.onrender.com",
     credentials: true,
   })
 );
